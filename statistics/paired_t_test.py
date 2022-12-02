105bb2d2c5703ddaadd00df67010c06994f9d803
Maciej Milanowski <87851852+Milanomac@users.noreply.github.com>
2022-12-02 10:10:22 +0100

    Update paired_t_test.py

--- statistics/paired_t_test.py
+++ statistics/paired_t_test.py
@@ -1,3 +1,8 @@
+# In some cases, you might be interested in testing differences between samples of the same group 
+# at different points in time. For instance, a hospital might want to test whether a weight-loss drug 
+# works by checking the weights of the same group patients before and after treatment. 
+# A paired t-test lets you check whether the means of samples from the same group differ.
+
 from scipy import stats
 import pandas as pd
 import numpy as np
@@ -18,6 +23,7 @@ print(weight_df.describe())            #check a summary of the data
 
 # print(weight_df)
 
+# Conducting a paired t-test to see whether this difference is significant at a 95% confidence level
 t_test = stats.ttest_rel(a = before,
                          b = after)
 
