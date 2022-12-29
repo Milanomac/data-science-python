89a9186025118c59206625104729943f83c555ea
milanomac <maciej.milanowsky@gmail.com>
2022-12-29 18:00:47 +0100

    Adding beginner-projects folder mainly games made for termial. First game is madlips

---
+++ beginner-projects/1_madlibs.py
@@ -0,0 +1,8 @@
+#how to string concatinate in an effective way
+#the easiest way to do this is by doing
+
+verb = input("Verb: ")
+adjective = input("Adjective: ")
+noun = input("Place: ")
+madlib = f"Programming is so {adjective} because it allows you to {verb}. I would rather do programming then going on vacation to {noun}."
+print(madlib)
